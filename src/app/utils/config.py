91c7f9fa2dd0c7f27e0936d1d7cbe70e8a3697d5
wicaksono <wcksnlxn@gmail.com>
2024-10-03 08:34:21 +0700

    abcd

--- src/app/utils/config.py
+++ src/app/utils/config.py
@@ -6,11 +6,9 @@ class Config:
     @staticmethod
     def init_firebase():
         try:
-            # Try to get the default app
             firebase_admin.get_app()
             print("Firebase app already initialized")
         except ValueError:
-            # The default app is not initialized yet, initialize it
             current_dir = os.path.dirname(os.path.abspath(__file__))
             cred_path = os.path.join(current_dir, '..', '..', 'db', 'FirebaseCred.json')
 
